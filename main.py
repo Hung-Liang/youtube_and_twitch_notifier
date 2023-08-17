aa1b8284ababe026c8d4ba3ddf7495774dda4fd2
Hung <zxjonathanzx@gmail.com>
2023-08-17 18:43:16 +0800

    add different group support

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from time import sleep, time
 from lib.utils.notify import send_notify
 from lib.utils.tools import send_exception_log, send_daily_log
 
+notify_list = [
+    ("", "group_1"),
+    ("", "group_1"),
+    ("", "group_2"),
+    ("", "group_3"),
+    ("", "group_3"),
+    ("", "group_3"),
+]
+
 
 while True:
     sleep(180 - time() % 180)
 
     try:
         send_daily_log()
-        send_notify("")
+
+        if time() % 360 <= 10:
+            for channel_id, group in notify_list:
+                send_notify(channel_id, group)
+        else:
+            for channel_id, group in notify_list[:2]:
+                send_notify(channel_id, group)
 
     except Exception as e:
         send_exception_log(e)
